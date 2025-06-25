82230432df69364d1c5bd3e435a137b08a11f727
yaago.souza <yaago.souza@hotmail.com>
2025-06-25 16:01:00 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Procura por classes em todos os arquivos JS/JSX da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
